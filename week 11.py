26279f7bc3b05506c71770d4ede52f06f9e8606c
bicrupeshkumar2025-collab <bicrupeshkumar2025@gmail.com>
2026-02-05 12:32:32 +0000

    Add files via upload

---
+++ week 11.py
@@ -0,0 +1,40 @@
+""" print("Hello, Level 4!")
+
+a = 10
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b) """
+
+
+""" name = input("Enter your name: ")
+print("Welcome,", name) """
+
+
+""" num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd") """
+
+
+""" for i in range(1, 6):
+    print("Number:", i) """
+
+
+
+"""     total = 0
+for i in range(1, 11):
+    total += i
+print("Sum =", total) """
+
+
+""" def greet(name):
+    print("Hello", name)
+
+greet("Rupesh") """
+
+
+ 
+print("Hello World")
